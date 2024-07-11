64662832c31424f1ec8abe817b3714d06613e176
Naren <alekhyabonkuri96@gmail.com>
2024-07-11 18:12:51 -0400

    mongo setup

--- mongo.sh
+++ mongo.sh
@@ -1,6 +1,6 @@
 Cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-systemctl enable mongod
-systemctl start mongod
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongo.conf
+systemctl enable mongo
+systemctl restart mongo
 
