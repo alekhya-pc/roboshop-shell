c54255b8992b7f95da15e562dbcac643f6b95f79
Naren <alekhyabonkuri96@gmail.com>
2024-07-12 12:50:57 -0400

    variable changes

--- common.sh
+++ common.sh
@@ -2,4 +2,16 @@ nodejs() {
   dnf module disable nodejs -y
   dnf module enable nodejs:20 -y
   dnf install nodejs -y
+  cp ${component}.service /etc/systemd/system/${component}.service
+  cp mongo.repo /etc/yum.repos.d/mongo.repo
+  useradd roboshop
+  rm -rf /app
+  mkdir /app
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
+  cd /app
+  unzip /tmp/${component}.zip
+  npm install
+  systemctl daemon-reload
+  systemctl enable ${component}
+  systemctl start ${component}
 }
