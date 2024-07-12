2a271daf122932f2bc4cba3448d4356fa2d9ec4a
Naren <alekhyabonkuri96@gmail.com>
2024-07-12 15:51:28 -0400

    shell script

--- catalogue.sh
+++ catalogue.sh
@@ -2,6 +2,6 @@ source common.sh
 component=catalogue
 nodejs
 echo Install Mongodb Client
-dnf install mongodb-mongosh -y &>$LOG_FILE
+dnf install mongodb-mongosh -y &>>$LOG_FILE
 echo Load Master Data
-mongosh --host mongo.dev.alekhyab96.online </app/db/master-data.js &>$LOG_FILE
+mongosh --host mongo.dev.alekhyab96.online </app/db/master-data.js &>>$LOG_FILE
--- common.sh
+++ common.sh
@@ -1,42 +1,42 @@
 LOG_FILE=/tmp/roboshop.log
 nodejs() {
   echo Disable nodejs Default Version
-  dnf module disable nodejs -y &>$LOG_FILE
+  dnf module disable nodejs -y &>>$LOG_FILE
 
   echo Enable nodejs 20 Module
-  dnf module enable nodejs:20 -y &>$LOG_FILE
+  dnf module enable nodejs:20 -y &>>$LOG_FILE
 
   echo Install nodejs
-  dnf install nodejs -y &>$LOG_FILE
+  dnf install nodejs -y &>>$LOG_FILE
 
   echo Copy service file
-  cp ${component}.service /etc/systemd/system/${component}.service &>$LOG_FILE
+  cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
 
   echo Copy MongoDB repo file
-  cp mongo.repo /etc/yum.repos.d/mongo.repo &>$LOG_FILE
+  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 
   echo Adding Application user
-  useradd roboshop &>$LOG_FILE
+  useradd roboshop &>>$LOG_FILE
 
   echo clearing Old content
-  rm -rf /app &>$LOG_FILE
+  rm -rf /app &>>$LOG_FILE
 
   echo create App Directory
-  mkdir /app &>$LOG_FILE
+  mkdir /app &>>$LOG_FILE
 
   echo Download App Content
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>$LOG_FILE
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
 
-  cd /app &>$LOG_FILE
+  cd /app &>>$LOG_FILE
 
   echo extract App Content
-  unzip /tmp/${component}.zip &>$LOG_FILE
+  unzip /tmp/${component}.zip &>>$LOG_FILE
 
   echo download nodejs Dependancies
-  npm install &>$LOG_FILE
+  npm install &>>$LOG_FILE
 
   echo Start Service
-  systemctl daemon-reload &>$LOG_FILE
-  systemctl enable ${component} &>$LOG_FILE
-  systemctl start ${component} &>$LOG_FILE
+  systemctl daemon-reload &>>$LOG_FILE
+  systemctl enable ${component} &>>$LOG_FILE
+  systemctl start ${component} &>>$LOG_FILE
 }
