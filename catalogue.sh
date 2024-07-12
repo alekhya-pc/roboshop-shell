10e524de1b1c3035825be1eac458dca22899618c
Naren <alekhyabonkuri96@gmail.com>
2024-07-12 15:41:37 -0400

    shell script

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,5 @@
 source common.sh
 component=catalogue
 nodejs
-dnf install mongodb-mongosh -y
-mongosh --host mongo.dev.alekhyab96.online </app/db/master-data.js
+dnf install mongodb-mongosh -y &>$LOG_FILE
+mongosh --host mongo.dev.alekhyab96.online </app/db/master-data.js &>$LOG_FILE
