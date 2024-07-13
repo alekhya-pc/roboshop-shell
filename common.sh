844ee0bf3202ae05fdde30a74185ae43544de7ba
Naren <alekhyabonkuri96@gmail.com>
2024-07-12 21:35:35 -0400

    shell script

--- common.sh
+++ common.sh
@@ -7,104 +7,65 @@ PRINT() {
   echo" ######################## $* ############################" &>>$LOG_FILE
   echo $*
 }
-nodejs() {
-  PRINT Disable nodejs Default Version
-  dnf module disable nodejs -y &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
+
+STAT() {
+  if [ $1 -eq 0 ]; then
     echo SUCCESS
   else
     echo FAILURE
   fi
+}
+
+nodejs() {
+  PRINT Disable nodejs Default Version
+  dnf module disable nodejs -y &>>$LOG_FILE
+  STAT $?
 
   PRINT Enable nodejs 20 Module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+ STAT $?
 
   PRINT Install nodejs
   dnf install nodejs -y &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  STAT $?
 
   PRINT Copy service file
   cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  STAT $?
 
   PRINT Copy MongoDB repo file
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  STAT $?
 
   PRINT Adding Application user
   useradd roboshop &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  STAT $?
 
   PRINT clearing Old content
   rm -rf /app &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  STAT $?
 
   PRINT create App Directory
   mkdir /app &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  STAT $?
 
   PRINT Download App Content
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  STAT $?
 
   cd /app &>>$LOG_FILE
 
   PRINT extract App Content
   unzip /tmp/${component}.zip &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  STAT $?
 
   PRINT download nodejs Dependancies
   npm install &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  STAT $?
 
   PRINT Start Service
   systemctl daemon-reload &>>$LOG_FILE
   systemctl enable ${component} &>>$LOG_FILE
   systemctl start ${component} &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  STAT $?
 }
