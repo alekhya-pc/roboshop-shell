93cc03cd79803c0a6f2cf8170f2ccac8a42fe680
Naren <alekhyabonkuri96@gmail.com>
2024-07-12 21:21:40 -0400

    shell script

--- catalogue.sh
+++ catalogue.sh
@@ -3,7 +3,15 @@ component=catalogue
 nodejs
 echo Install Mongodb Client
 dnf install mongodb-mongosh -y &>>$LOG_FILE
-echo $?
+if [$? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 echo Load Master Data
 mongosh --host mongo.dev.alekhyab96.online </app/db/master-data.js &>>$LOG_FILE
-echo $?
+if [$? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
--- common.sh
+++ common.sh
@@ -10,53 +10,101 @@ PRINT() {
 nodejs() {
   PRINT Disable nodejs Default Version
   dnf module disable nodejs -y &>>$LOG_FILE
-  echo $?
+  if [$? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   PRINT Enable nodejs 20 Module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
-  echo $?
+  if [$? -eq 0]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
 
   PRINT Install nodejs
   dnf install nodejs -y &>>$LOG_FILE
-  echo $?
+  if [$? -eq 0]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
 
   PRINT Copy service file
   cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
-  echo $?
+  if [$? -eq 0]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
 
   PRINT Copy MongoDB repo file
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
-  echo $?
+  if [$? -eq 0]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
 
   PRINT Adding Application user
   useradd roboshop &>>$LOG_FILE
-  echo $?
+  if [$? -eq 0]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
 
   PRINT clearing Old content
   rm -rf /app &>>$LOG_FILE
-  echo $?
+  if [$? -eq 0]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
 
   PRINT create App Directory
   mkdir /app &>>$LOG_FILE
-  echo $?
+  if [$? -eq 0]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
 
   PRINT Download App Content
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
-  echo $?
+  if [$? -eq 0]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
 
   cd /app &>>$LOG_FILE
 
   PRINT extract App Content
   unzip /tmp/${component}.zip &>>$LOG_FILE
-  echo $?
+  if [$? -eq 0]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
 
   PRINT download nodejs Dependancies
   npm install &>>$LOG_FILE
-  echo $?
+  if [$? -eq 0]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
 
   PRINT Start Service
   systemctl daemon-reload &>>$LOG_FILE
   systemctl enable ${component} &>>$LOG_FILE
   systemctl start ${component} &>>$LOG_FILE
-  echo $?
+  if [$? -eq 0]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
 }
