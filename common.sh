9ca2a492a00378701303b21f11acaf2d8e1cd53a
Naren <alekhyabonkuri96@gmail.com>
2024-07-12 23:35:05 -0400

    shell script

--- common.sh
+++ common.sh
@@ -5,8 +5,6 @@ PRINT() {
   echo &>>$LOG_FILE
   echo &>>$LOG_FILE
   echo " ######################## $* ############################" &>>$LOG_FILE
-  echo
-  echo "Refer the log file for more information : File path : ${LOG_FILE}"
   echo $*
 }
 
@@ -15,6 +13,8 @@ STAT() {
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
+    echo
+    echo "Refer the log file for more information : File path : ${LOG_FILE}"
     exit $1
   fi
 }
