01ed5c81dee468ecf9704900aa26f303ba3a960e
Naren <alekhyabonkuri96@gmail.com>
2024-07-13 01:36:29 -0400

    shell script

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,14 @@
-dnf install mysql-server -y
-systemctl enable mysqld
-systemctl restart mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+source common.sh
+
+PRINT Install MySql Service
+dnf install mysql-server -y &>>$LOG_FILE
+STAT $?
+
+PRINT Start MySql Service
+systemctl enable mysqld &>>$LOG_FILE
+systemctl restart mysqld &>>$LOG_FILE
+STAT $?
+
+PRINT Setup Mysql root password
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
+STAT $?
