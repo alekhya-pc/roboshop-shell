70f8cdcdafd4ac90020668837af4693951086547
Naren <alekhyabonkuri96@gmail.com>
2024-07-14 12:45:50 -0400

    shell script

--- payment.sh
+++ payment.sh
@@ -4,13 +4,13 @@ app_path=/app
 
 
 PRINT install python
-dnf install python3 gcc python3-devel -y
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
 STAT $?
 
 APP_PREREQ
 
-PRINT install requirements
-pip3 install -r requirements.txt
+PRINT install requirements &>>$LOG_FILE
+pip3 install -r requirements.txt &>>$LOG_FILE
 STAT $?
 
 SYSTEMD_SETUP
