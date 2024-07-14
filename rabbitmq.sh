87d9ed3c6c1225a4094a2345abf6472bdf43334c
Naren <alekhyabonkuri96@gmail.com>
2024-07-14 12:19:05 -0400

    shell script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,20 @@
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
-dnf install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+source common.sh
+component=rabbitmq
+
+PRINT Copy rabbitmq repo file
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+STAT $?
+
+PRINT Install rabbitmq
+dnf install rabbitmq-server -y &>>$LOG_FILE
+STAT $?
+
+PRINT Start rabbitmq service
+systemctl enable rabbitmq-server &>>$LOG_FILE
+systemctl restart rabbitmq-server &>>$LOG_FILE
+STAT $?
+
+PRINT Created one user for application
+rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
+STAT $?
