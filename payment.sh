cd6cf16f42d0c3f45e2037505d5b179e401559c0
Naren <alekhyabonkuri96@gmail.com>
2024-07-14 12:43:06 -0400

    shell script

--- payment.sh
+++ payment.sh
@@ -1,13 +1,17 @@
-cp payment.service /etc/systemd/system/payment.service
+source common.sh
+component=payment
+app_path=/app
+
+
+PRINT install python
 dnf install python3 gcc python3-devel -y
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
-cd /app
-unzip /tmp/payment.zip
-cd /app
+STAT $?
+
+APP_PREREQ
+
+PRINT install requirements
 pip3 install -r requirements.txt
-systemctl daemon-reload
-systemctl enable payment
-systemctl restart payment
+STAT $?
+
+SYSTEMD_SETUP
+
