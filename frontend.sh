4f55466e8dc93a0d2d32fee2987453b2aa46ccf7
Naren <alekhyabonkuri96@gmail.com>
2024-07-15 16:23:43 -0400

    file name change

--- frontend.sh
+++ frontend.sh
@@ -15,7 +15,7 @@ dnf install nginx -y &>>$LOG_FILE
 STAT $?
 
 PRINT copy nginx config file
-cp nginix.conf /etc/nginx/nginx.conf &>>$LOG_FILE
+cp nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
 STAT $?
 
 APP_PREREQ
